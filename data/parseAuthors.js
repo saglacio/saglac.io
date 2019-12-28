ff317b260bc96c06b75ad8bb460454cd029ca181
Emile Bergeron <ber.emile@gmail.com>
2019-12-28 03:21:14 -0500

    Adds parsed locations

--- data/parseAuthors.js
+++ data/parseAuthors.js
@@ -1,15 +1,15 @@
 const fs = require('fs');
 const YAML = require('yaml');
 
-const sourceFile = fs.readFileSync(`${__dirname}/authors.yml`, 'utf8');
+const sourceFile = fs.readFileSync(`${__dirname}/locations.yml`, 'utf8');
 const sourceData = YAML.parse(sourceFile);
 
 // console.log('TEST', sourceData);
 
-Object.keys(sourceData).forEach((authorId) => {
-  const author = sourceData[authorId];
-  fs.writeFile(`${__dirname}/authors/${authorId}.yml`, YAML.stringify({
-    id: authorId,
-    ...author,
+Object.keys(sourceData).forEach((locationId) => {
+  const location = sourceData[locationId];
+  fs.writeFile(`${__dirname}/locations/${locationId}.yml`, YAML.stringify({
+    id: locationId,
+    ...location,
   }), console.log);
 });
