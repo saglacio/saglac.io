367f0f19f1a525cbc43cbc7247723bfc6ccfa49d
Emile Bergeron <ber.emile@gmail.com>
2020-01-07 01:58:33 -0500

    Tweaks intro scetion

--- src/components/pages/IndexPage/IntroSection/IntroSection.jsx
+++ src/components/pages/IndexPage/IntroSection/IntroSection.jsx
@@ -1,17 +1,38 @@
 import React from 'react';
 // import Typo
 import './IntroSection.scss';
+import Container from 'reactstrap/lib/Container';
+import Row from 'reactstrap/lib/Row';
+import Col from 'reactstrap/lib/Col';
 import Typography from '@/components/shared/Typography';
+import Button from '@/components/shared/Button';
 
 const IntroSection = () => (
   <section id="intro" className="io-intro-section">
-    <div className="intro-container">
-      <Typography variant="h1">
-      Saglac IO
-      </Typography>
-      <Typography>
-      Rencontre technologique du Saguenay—Lac-St-Jean
-      </Typography>
+    <div className="intro-wrapper">
+      <Container>
+        <Row className="align-items-center justify-content-center">
+          <Col lg={{ size: 8 }} className="">
+
+            <Typography
+              variant="h1"
+              className="display-3 mb-5"
+              uppercase
+              light
+            >
+              Rencontre
+              <br />
+              technologique
+              <br />
+              <Typography tag="div" className="h2" uppercase>
+              du Saguenay—Lac-St-Jean
+              </Typography>
+            </Typography>
+            <Button size="lg">Participer!</Button>
+            <Button color="primary" size="lg">En savoir plus</Button>
+          </Col>
+        </Row>
+      </Container>
     </div>
   </section>
 );
