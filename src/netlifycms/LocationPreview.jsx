d1d3181a4bd8aa1cf5539e19fafa75b515fe221f
Emile Bergeron <ber.emile@gmail.com>
2022-03-20 20:50:32 -0400

    Remove debug commented code

--- src/netlifycms/LocationPreview.jsx
+++ src/netlifycms/LocationPreview.jsx
@@ -2,10 +2,7 @@ import React from 'react';
 import LocationInfo from '@/components/shared/LocationInfo';
 
 const LocationPreview = ({ entry }) => {
-  console.log('DEBUG', entry);
-  // const data = entry.getIn(['data']);
   const data = entry.get('data').toObject();
-  console.log('DEBUG', data);
   return <LocationInfo location={data} />;
 };
 
