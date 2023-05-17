c175a8cc08404903b898d97148f986b5f0f52cff
Gabriel Le Breton <lebreton.gabriel@gmail.com>
2023-05-17 00:02:07 -0400

    Add anchor links and expand/collapse functionality to FAQ questions

--- src/components/pages/FAQPage/FAQPage.jsx
+++ src/components/pages/FAQPage/FAQPage.jsx
@@ -1,52 +1,63 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactMarkdown from 'react-markdown';
-import PageLayout from '@/components/layout/PageLayout';
 import { Card, CardBody, CardHeader, Collapse } from 'reactstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronDown, faLink } from '@fortawesome/free-solid-svg-icons';
+import Row from 'reactstrap/lib/Row';
+import Col from 'reactstrap/lib/Col';
+import PageLayout from '@/components/layout/PageLayout';
 import './FAQPage.scss';
 
 const FAQPage = ({ sections }) => {
-  const [openCards, setOpenCards] = useState([]);
+  const [openedQuestion, setOpenedQuestion] = useState(null);
 
-  const toggleCard = (index) => {
-    const newState = [...openCards];
+  useEffect(() => {
+    setOpenedQuestion(window.location.hash.substring(1));
+  }, []);
 
-    if (newState.includes(index)) {
-      const position = newState.indexOf(index);
-      newState.splice(position, 1);
-    } else {
-      newState.push(index);
-    }
-
-    setOpenCards(newState);
+  const toggleQuestion = (slug) => {
+    setOpenedQuestion(openedQuestion === slug ? null : slug);
   };
-
   return (
     <PageLayout title="FAQ">
-      <div className="faq-container">
-        {sections.map((section, sectionIndex) => (
-          <div key={section.header} className="faq-section">
-            <h2>{section.header}</h2>
-            <div className="faq-cards">
-              {section.questions.map((question, questionIndex) => {
-                const cardIndex = `${sectionIndex}-${questionIndex}`;
-                const isOpen = openCards.includes(cardIndex);
-                return (
-                  <Card key={question.slug} className="faq-question">
-                    <CardHeader onClick={() => toggleCard(cardIndex)}>
-                      <ReactMarkdown as="h3">{question.question}</ReactMarkdown>
-                      <div className={`arrow ${isOpen ? 'open' : ''}`} />
+      <div className="faq-container container">
+        <Row>
+          {sections.map((section) => (
+            <Col key={section.header} sm="12" md="6">
+              <div className="faq-section">
+                <h2>{section.header}</h2>
+                {section.questions.map((question) => (
+                  <Card
+                    key={question.slug}
+                    className="faq-question"
+                    id={question.slug}
+                  >
+                    <CardHeader
+                      onClick={() => toggleQuestion(question.slug)}
+                      className="header-container"
+                    >
+                      <a href={`#${question.slug}`} className="anchor">
+                        <FontAwesomeIcon icon={faLink} />
+                      </a>
+                      <div className="question-text">
+                        <ReactMarkdown>{question.question}</ReactMarkdown>
+                      </div>
+                      <FontAwesomeIcon
+                        icon={faChevronDown}
+                        className={`expand-icon ${openedQuestion === question.slug ? 'open' : ''}`}
+                      />
                     </CardHeader>
-                    <Collapse isOpen={isOpen}>
+                    <Collapse isOpen={openedQuestion === question.slug}>
                       <CardBody>
                         <ReactMarkdown>{question.answer}</ReactMarkdown>
                       </CardBody>
                     </Collapse>
                   </Card>
-                );
-              })}
-            </div>
-          </div>
-        ))}
+                ))}
+              </div>
+            </Col>
+          ))}
+        </Row>
       </div>
     </PageLayout>
   );
