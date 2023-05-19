5af5fe38a99a5a8361af0a320aefb754f58c9970
Gabriel Le Breton <lebreton.gabriel@gmail.com>
2023-05-18 20:28:02 -0400

    Refactor ArchivesPage to own componentˆ

--- src/pages/archives.jsx
+++ src/pages/archives.jsx
@@ -33,31 +33,10 @@ export const query = graphql`
 
 const ArchivesPage = ({ data }) => {
   const {
-    allIoEventsYaml: { nodes: events },
+    allIoEventsYaml: { nodes },
   } = data;
 
-  return (
-    <Layout title="Archives">
-      <section className="container">
-        {events.map((event) => (
-          <div key={event.id}>
-            <h2>{event.title}</h2>
-            <p>Date: {new Date(event.date).toLocaleDateString()}</p>
-            <p>Lieu: {event.location.name}</p>
-            {event.talks.map((talk) => (
-              <div key={talk.id}>
-                <h3>{talk.title}</h3>
-                <p>
-                  Auteur: {talk.authors.map((author) => author.name).join(', ')}
-                </p>
-              </div>
-            ))}
-            <Link to={event.event_url}>Lien vers l'évènement</Link>
-          </div>
-        ))}
-      </section>
-    </Layout>
-  );
+  return <ArchivesPage events={nodes} />;
 };
 
 export default ArchivesPage;
