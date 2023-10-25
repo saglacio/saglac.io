c967b1431feb40d6a8ff473cc38d528ed90fe5d1
Gabriel Le Breton <lebreton.gabriel@gmail.com>
2023-10-25 13:55:26 -0400

    WIP: use ? everywhere as failsafe, but that's not good

--- src/components/pages/ArchivesPage/ArchivesPage.jsx
+++ src/components/pages/ArchivesPage/ArchivesPage.jsx
@@ -10,12 +10,12 @@ const ArchivesPage = ({ events }) => (
         <div key={event.id}>
           <h2>{event.title}</h2>
           <p>Date: {new Date(event.date).toLocaleDateString()}</p>
-          <p>Lieu: {event.location.name}</p>
-          {event.talks.map((talk) => (
-            <div key={talk.id}>
-              <h3>{talk.title}</h3>
+          <p>Lieu: {event?.location?.name}</p>
+          {event?.talks?.map((talk) => (
+            <div>
+              <h3>{talk?.title}</h3>
               <p>
-                Auteur: {talk.authors.map((author) => author.name).join(', ')}
+                Auteur: {talk?.authors.map((author) => author.name).join(', ')}
               </p>
             </div>
           ))}
