b68bcdfdbb51d30a2b4516113f99160c5fcde201
JP Boily <jp@metrics.watch>
2023-10-25 14:43:00 -0400

    prevent build crashing if an event had no talks

--- src/components/pages/ArchivesPage/ArchivesPage.jsx
+++ src/components/pages/ArchivesPage/ArchivesPage.jsx
@@ -11,8 +11,8 @@ const ArchivesPage = ({ events }) => (
           <h2>{event.title}</h2>
           <p>Date: {new Date(event.date).toLocaleDateString()}</p>
           <p>Lieu: {event?.location?.name}</p>
-          {event?.talks?.map((talk) => (
-            <div>
+          {event?.talks?.map((talk, index) => (
+            <div key={`event-${index}`}>
               <h3>{talk?.title}</h3>
               <p>
                 Auteur: {talk?.authors.map((author) => author.name).join(', ')}
