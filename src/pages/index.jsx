9eb072a6a0c142f78b161d3a128055c0b2c0d386
Gabriel Le Breton <lebreton.gabriel@gmail.com>
2023-10-24 22:55:18 -0400

    revert npx gatsby-codemods@latest sort-and-aggr-graphql .

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,55 +1,53 @@
-const _interopRequireDefault = require('@babel/runtime/helpers/interopRequireDefault');
-exports.__esModule = true;
-exports.default = Index;
-exports.pageQuery = void 0;
-const _react = _interopRequireDefault(require('react'));
-const _gatsby = require('gatsby');
-const _IndexPage = _interopRequireDefault(
-  require('@/components/pages/IndexPage'),
-);
+import React from 'react';
+import { graphql } from 'gatsby';
+import IndexPage from '@/components/pages/IndexPage';
 
-function Index({
+export default function Index({
   data: {
     latestEvent: {
       nodes: [latestEvent],
     },
   },
 }) {
-  return _react.default.createElement(_IndexPage.default, {
-    event: latestEvent,
-  });
+  return <IndexPage event={latestEvent} />;
 }
 
-const pageQuery = (0, _gatsby.graphql)`query LatestEvent {
-  latestEvent: allIoEventsYaml(sort: {date: DESC}, limit: 1) {
-    totalCount
-    nodes {
-      id
-      title
-      description
-      formattedDate: date(formatString: "dddd DD MMMM YYYY, [à] LT", locale: "fr")
-      date
-      event_url
-      talks {
+export const pageQuery = graphql`
+  query LatestEvent {
+    latestEvent: allIoEventsYaml(
+      sort: { fields: date, order: DESC }
+      limit: 1
+    ) {
+      totalCount
+      nodes {
+        id
         title
         description
-        authors {
+        formattedDate: date(
+          formatString: "dddd DD MMMM YYYY, [à] LT"
+          locale: "fr"
+        )
+        date
+        event_url
+        talks {
+          title
+          description
+          authors {
+            id
+            name
+            website
+          }
+        }
+        location {
           id
           name
-          website
+          address
+          url
+          facebook
+          description
+          map
         }
-      }
-      location {
-        id
-        name
-        address
-        url
-        facebook
-        description
-        map
       }
     }
   }
-}`;
-
-exports.pageQuery = pageQuery;
+`;
