5840ee5a1da9cafd1232f25be37a3131fb60908c
Gabriel Le Breton <lebreton.gabriel@gmail.com>
2024-06-06 08:52:05 -0400

    npm run lint:fix

--- src/components/pages/ArchivesPage/ArchivesPage.jsx
+++ src/components/pages/ArchivesPage/ArchivesPage.jsx
@@ -1,8 +1,8 @@
-import React from "react";
-import { Link } from "gatsby";
-import Layout from "@/components/layout/PageLayout/PageLayout";
-import Section from "@/components/shared/Section";
-import "./ArchivesPage.scss";
+import React from 'react';
+import { Link } from 'gatsby';
+import Layout from '@/components/layout/PageLayout/PageLayout';
+import Section from '@/components/shared/Section';
+import './ArchivesPage.scss';
 
 const ArchivesPage = ({ events }) => (
   <Layout title="Archives">
@@ -10,21 +10,27 @@ const ArchivesPage = ({ events }) => (
       {events.map((event) => (
         <div className="event-card" key={event.id}>
           <h2>{event.title}</h2>
-          <p className="event-date">Date: {new Date(event.date).toLocaleDateString()}</p>
+          <p className="event-date">
+            Date: {new Date(event.date).toLocaleDateString()}
+          </p>
           <p className="event-location">Lieu: {event?.location?.name}</p>
           {event?.talks?.map((talk, index) => (
             <div className="talk-card" key={`event-${index}`}>
               <h3>{talk?.title}</h3>
               <p className="talk-author">
-                Personne présentant: {talk?.authors?.name || (
-                <span className="error-message">
-                    ERREUR: veuillez ajouter le fichier `.yml` pour cette personne dans le répertoire `data/authors`.
+                Personne présentant:{' '}
+                {talk?.authors?.name || (
+                  <span className="error-message">
+                    ERREUR: veuillez ajouter le fichier `.yml` pour cette
+                    personne dans le répertoire `data/authors`.
                   </span>
-              )}
+                )}
               </p>
             </div>
           ))}
-          <Link className="event-link" to={event.event_url}>Lien vers l'évènement</Link>
+          <Link className="event-link" to={event.event_url}>
+            Lien vers l'évènement
+          </Link>
         </div>
       ))}
     </Section>
