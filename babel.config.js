3fcfffcec9555b6ee6053d39141af1a6fb230226
Gabriel Le Breton <lebreton.gabriel@gmail.com>
2025-10-14 16:23:55 -0400

    Delete older website (bye)

--- babel.config.js
+++
@@ -1,6 +0,0 @@
-const resolver = require('./resolver.config');
-
-module.exports = {
-  presets: ['babel-preset-gatsby'],
-  plugins: [['module-resolver', resolver]],
-};
--- sass-loader.config.js
+++
@@ -1,9 +0,0 @@
-// Since this is used by both the gatsby webpack config and the storybook.
-module.exports = {
-  // Looks like it's relative to the CWD of the process rather than the file
-  // it's imported into.
-  additionalData: '@import "./src/scss/modules";',
-  sassOptions: {
-    precision: 6, // recommended for Bootstrap 4
-  },
-};
